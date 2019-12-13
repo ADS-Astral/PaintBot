1a322543be3c439de847ef28766ec3f194674f60
Yvan Burrie <yvan.burrie@hotmail.com>
2019-12-13 14:29:30 +1000

    Move RS script to seperate file

--- App.py
+++ App.py
@@ -97,26 +97,3 @@ import numpy as np
 #     app.MainLoop()
 
 
-# wx and rs example
-app = wx.App()
-window = wx.Frame(None, title="wxPython Frame", size=(800, 600))
-panel = wx.Panel(window)
-
-pipe = rs.pipeline()
-profile = pipe.start()
-try:
-    msg = ""
-    for i in range(0, 100):
-        frames = pipe.wait_for_frames()
-        for f in frames:
-            msg += "{} {} {} {}".format(
-                f.profile.stream_index(),
-                f.profile.stream_name(),
-                f.profile.stream_type(),
-                f.profile.unique_id()) + "\n"
-    label = wx.StaticText(panel, label=msg, pos=(100, 50), size=(500, 500))
-finally:
-    pipe.stop()
-
-window.Show(True)
-app.MainLoop()
