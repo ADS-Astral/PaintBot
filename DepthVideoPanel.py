904f9d39983e1c6c1d084f25fb853e9ba40215f8
brandon <brandon.johns@my.jcu.edu.au>
2019-12-19 11:11:58 +1000

    Add constants for centre of screen

--- DepthVideoPanel.py
+++ DepthVideoPanel.py
@@ -4,6 +4,9 @@ import numpy as np
 import pyrealsense2 as rs
 from VideoPanel import VideoPanel
 
+# x, y coordinates for the point on the video stream to detect distance
+X = 320
+Y = 240
 
 class DepthVideoPanel(VideoPanel):
 
@@ -19,15 +22,17 @@ class DepthVideoPanel(VideoPanel):
 
     def GetDataBuffer(self):
         # Start streaming
+        frames = self.pipeline.wait_for_frames(1000)
+
+        # get distance to object in center of camera feed
         depth_sensor = profile.get_device().first_depth_sensor()
         depth_scale = depth_sensor.get_depth_scale()
-        frames = self.pipeline.wait_for_frames(1000)
         depth_frame = frames.get_depth_frame()
         depth_image = np.asanyarray(depth_frame.get_data())
-        depth = depth_image[320,240].astype(float)
+        depth = depth_image[Y, X].astype(float)
         distance = depth * depth_scale
-
         print ("Distance (m):  {:.2f}".format(distance))
+
         dst = None
         depth_colormap = cv.applyColorMap(cv.convertScaleAbs(depth_image, alpha=self.distance), colormap=self.colormap, dst=dst)
         buffer = cv.cvtColor(depth_colormap, cv.COLOR_BGR2RGB)
@@ -40,8 +45,6 @@ class DepthVideoPanel(VideoPanel):
 
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 profile = pipeline.start(config)
 
 if __name__ == '__main__':
