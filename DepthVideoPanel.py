135c087a972089a7ec07363162b15b0b0996f867
Yvan Burrie <yvan.burrie@hotmail.com>
2019-12-19 11:30:21 +1000

    Update DepthVideoPanel.py

--- DepthVideoPanel.py
+++ DepthVideoPanel.py
@@ -4,36 +4,41 @@ import numpy as np
 import pyrealsense2 as rs
 from VideoPanel import VideoPanel
 
-# x, y coordinates for the point on the video stream to detect distance
-X = 320
-Y = 240
 
 class DepthVideoPanel(VideoPanel):
 
     pipeline = None
+    config = None
+    profile = None
     distance = 0.08
     colormap = cv.COLORMAP_BONE
 
-    def __init__(self, parent, pipeline):
+    def __init__(self, parent, pipeline, config):
         self.pipeline = pipeline
+        self.config = config
+        self.profile = pipeline.start(config)
         VideoPanel.__init__(self, parent)
 
         pass
 
+    def __del__(self):
+        self.pipeline.stop()
+        pass
+
     def GetDataBuffer(self):
         # Start streaming
-        frames = self.pipeline.wait_for_frames(1000)
-
-        # get distance to object in center of camera feed
-        depth_sensor = profile.get_device().first_depth_sensor()
-        depth_scale = depth_sensor.get_depth_scale()
         try:
             frames = self.pipeline.wait_for_frames(1000)
         except RuntimeError:
             return None  # todo: error for frame not arriving within 1 sec
+
+        # Get distance to object in center of camera feed
+        depth_sensor = self.profile.get_device().first_depth_sensor()
+        depth_scale = depth_sensor.get_depth_scale()
         depth_frame = frames.get_depth_frame()
         depth_image = np.asanyarray(depth_frame.get_data())
-        depth = depth_image[Y, X].astype(float)
+        # Coordinates for the point on the video stream to detect distance
+        depth = depth_image[int(self.height / 2), int(self.width / 2)].astype(float)
         distance = depth * depth_scale
 
         print("Distance (m):  {:.2f}".format(distance))
@@ -47,16 +52,10 @@ class DepthVideoPanel(VideoPanel):
     pass  # DepthVideoPanel
 
 
-pipeline = rs.pipeline()
-config = rs.config()
-profile = pipeline.start(config)
-
 if __name__ == '__main__':
     app = wx.App()
     frame = wx.Frame(None)
-    panel = DepthVideoPanel(frame, pipeline)
+    panel = DepthVideoPanel(frame, rs.pipeline(), rs.config())
     frame.SetSize((panel.width, panel.height))
     frame.Show()
     app.MainLoop()
-
-pipeline.stop()
