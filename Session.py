b76182fb5449905536849b9b5c59ccb5b9eb59c5
Yvan Burrie <yvan.burrie@hotmail.com>
2020-01-10 13:44:58 +1000

    Update Session.py

--- Session.py
+++ Session.py
@@ -2,18 +2,25 @@ from datetime import datetime
 
 
 class Session:
+    """
+    A session is a job or task appointed to the robot.
+    The robot can remember what it was previously doing after turning it on or off.
+    """
 
     startTime = 0
     file = None
+
+    # Contains all the feeds in short-term memory.
     feeds = []
 
     def __init__(self):
         self.time = datetime.now()
-        self.file = open("session_{}.csv".format(self.time), 'a')
+        self.file = open(file="session_{}.csv".format(self.time).replace(":", "."), mode='a')
         pass
 
     def __del__(self):
-        self.file.close()
+        if self.file is not None:
+            self.file.close()
         pass
 
     def Append(self, feed):
@@ -29,13 +36,16 @@ class Session:
         """
         with self.file as appender:
             for feed in self.feeds:
-                appender.write(feed.ToRow())
-        self.feeds = []
+                appender.write(str(feed.ToRow()))
+        self.feeds.clear()
 
     pass  # class Session
 
 
 class Feed:
+    """
+    A feed is an event or action performed by or to the robot as an element of history for the session.
+    """
 
     distance = 0
     battery = 0
